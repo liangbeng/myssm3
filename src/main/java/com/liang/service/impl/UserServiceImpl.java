df1f71aea07797f806ad76b1ef41b3f3fd010a03
liangben <qxdz6dbx>
2021-03-03 18:34:53 +0800

    添加日志配置,有个问题日志并未输出到指定文件

--- src/main/java/com/liang/service/impl/UserServiceImpl.java
+++ src/main/java/com/liang/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.liang.service.impl;
 import com.liang.dao.UserDao;
 import com.liang.pojo.User;
 import com.liang.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +12,14 @@ import javax.annotation.Resource;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Resource
     private UserDao userDao;
 
     @Override
     public User findUserById(int id) {
+        logger.info("query user info;th id is:{}",id);
         return userDao.findUserById(id);
     }
 
@@ -23,6 +27,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public void insertUser(User user) {
         userDao.insertUser(user);
-
+        logger.info("add user success;th user is:{}",user);
     }
 }
